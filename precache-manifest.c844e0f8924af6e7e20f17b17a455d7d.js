bd8122e471acc5304e8b954d37d8d8eb7aae62ba
Ashwini Nair <ashwini.nair@seqato.com>
2020-04-04 22:47:56 +0530

    Updates

---
+++ precache-manifest.c844e0f8924af6e7e20f17b17a455d7d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7e34f6d0c2c312caab9ddf3fb2638a3d",
+    "url": "/realtime-document/index.html"
+  },
+  {
+    "revision": "349570171e071f827129",
+    "url": "/realtime-document/static/css/main.db9614d6.chunk.css"
+  },
+  {
+    "revision": "bf7f39fb97ddb1c98ee0",
+    "url": "/realtime-document/static/js/2.d834b141.chunk.js"
+  },
+  {
+    "revision": "884d6422099fcad6c31f436a36163b39",
+    "url": "/realtime-document/static/js/2.d834b141.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "349570171e071f827129",
+    "url": "/realtime-document/static/js/main.9360cedb.chunk.js"
+  },
+  {
+    "revision": "602778385040fc4544c8",
+    "url": "/realtime-document/static/js/runtime-main.3792190b.js"
+  }
+]);
